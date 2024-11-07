68c1c9c3fc559d2ba52519529dda9bcc2d2527ba
Dr. Taylor <taylor@msoe.edu>
2024-11-07 08:49:12 -0600

    Week 10 Lecture 3

--- src/wk10/Driver.java
+++ src/wk10/Driver.java
@@ -2,6 +2,34 @@ package wk10;
 
 public class Driver {
     public static void main(String[] args) {
+        int i = 3;
+        int j = 0;
+        String answer;
+        if (j != 0 && i / j > 8) {
+            answer = "hi";
+        } else {
+            answer = "bye";
+        }
+        System.out.println(answer);
+
+        // TERNARY
+        // CONDITIONAL ? <TRUE STATEMENT> : <FALSE STATEMENT>;
+        System.out.println((j != 0 && i / j > 8) ? "hi" : "bye");
+
+        String word = null;
+        if (word != null && word.length() > 4) {
+            System.out.println("Nice letters");
+        }
+
+        if (true || false) {
+
+        }
+        char a = 'p';
+        System.out.println(a++ - 'a' + 1);
+        System.out.println(a);
+    }
+
+    public static void main2(String[] args) {
         System.out.println(Color.RED + "this is red");
         System.out.println("\u001B[1m this is red");
         System.out.println(Color.RESET + "this is red");
