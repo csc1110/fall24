6984f3b65bdad23dcd3f4d6288b50e16c67676ba
Dr. Taylor <taylor@msoe.edu>
2024-12-02 09:02:27 -0600

    Week 14 Lecture 1

--- src/wk14/Driver.java
+++ src/wk14/Driver.java
@@ -1,22 +1,33 @@
 package wk14;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.zip.ZipInputStream;
 
 public class Driver {
-    public static void main(String[] args) {
-        Path path = Path.of(".gitignore");
-        System.out.println(path.getFileName());
-        System.out.println(path.getName(0));
-        Path abs = path.toAbsolutePath();
-        System.out.println(abs);
-        try {
-            List<String> lines = Files.readAllLines(path);
-            System.out.println(lines.get(7));
-        } catch (IOException e) {
-            System.out.println("Unable to read file");
+    public static void main(String[] args) throws IOException {
+        Path path = Path.of("file.txt");
+        writeDouble(path, 1.1);
+        writeDouble(path, 1.2);
+        writeDouble(path, 1.3);
+        writeDouble(path, 1.4);
+        writeDouble(path, 1.5);
+        System.out.println(readDouble(path));
+    }
+
+    public static void writeDouble(Path path, double data) throws IOException {
+        OutputStream out = Files.newOutputStream(path, StandardOpenOption.APPEND);
+        DataOutputStream dOut = new DataOutputStream(out);
+        dOut.writeDouble(data);
+        dOut.writeChars("happy");
+    }
+    public static double readDouble(Path path) throws IOException {
+        try (DataInputStream in = new DataInputStream(new
+                BufferedInputStream(new ZipInputStream(Files.newInputStream(path))))) {
+            return in.readDouble();
         }
     }
 }
