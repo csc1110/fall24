7ed77ee80886e62bce6156d282beedc93eb01363
Dr. Taylor <taylor@msoe.edu>
2024-12-03 09:00:39 -0600

    Week 14 Lecture 2 activity 2

--- src/wk14/Exercise.java
+++ src/wk14/Exercise.java
@@ -1,5 +1,6 @@
 package wk14;
 
+import java.io.DataOutputStream;
 import java.io.IOError;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,15 +10,15 @@ import java.util.Scanner;
 
 public class Exercise {
     public static void main(String[] ignored) {
-        System.out.println("Enter 3 integers");
+        System.out.println("Enter 3 doubles");
         Scanner in = new Scanner(System.in);
-        int first = in.nextInt();
-        int middle = in.nextInt();
-        int last = in.nextInt();
-        try (PrintWriter out = new PrintWriter(Files.newOutputStream(Path.of("ints.txt")))) {
-            out.println(first);
-            out.println(middle);
-            out.println(last);
+        double first = in.nextDouble();
+        double middle = in.nextDouble();
+        double last = in.nextDouble();
+        try (DataOutputStream out = new DataOutputStream(Files.newOutputStream(Path.of("doubles.bin")))) {
+            out.writeDouble(first);
+            out.writeDouble(middle);
+            out.writeDouble(last);
         } catch(IOException e) {
             System.out.println("Error writing to the file");
         }
